3935e896743b92fdf45a46ec85db1f54c23c2d39
EphronM <ephronmartin2016@gmail.com>
2022-03-26 17:27:58 +0530

    data loaded

---
+++ src/data/load_data.py
@@ -0,0 +1,30 @@
+import yaml
+import argparse
+import pandas as pd
+from src.config import read_yaml
+
+
+
+
+def load_data(data_path):
+    df = pd.read_csv(data_path, encoding='utf-8')
+    return df
+
+def load_raw_data(config_path):
+    config=read_yaml(config_path)
+
+    external_data_path=config["external_data_source"]["cassendra_db"]
+    raw_data_path=config["raw_data_config"]["raw_data_csv"]
+    
+    df = load_data(external_data_path)
+    df.to_csv(raw_data_path,index=False)
+
+
+
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    load_raw_data(config_path=parsed_args.config)
